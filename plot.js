86b1a92c474e86bbe2f3c2cc4e255da968c8627c
karahodges <61912409+karahodges@users.noreply.github.com>
2020-07-09 19:57:29 -0500

    Add files via upload

--- plot.js
+++ plot.js
@@ -1,72 +1,69 @@
-d3.csv("cleanData.csv").then(function(data) {
-    console.log('works');
-  });
+var country = [];
+var incometaxrate= [];
+var corptaxrate= [];
+var taxburden= [];
 
+Plotly.d3.csv("cleanData.csv", function(kara){ 
+    for (var i = 0; i < kara.length; i++) {
+      row = kara[i];
+      country.push(row['Country Name']);
+      incometaxrate.push(row['Income Tax Rate (%)']);
+      corptaxrate.push(row['Corporate Tax Rate (%)']);
+      taxburden.push(row['Tax Burden % of GDP']);
+    }});
 
-data.forEach(function(data) {
-    data.incometaxrate = +data.incometaxrate;
-    data.country = +data.country;
-    data.corptaxrate= +data.corptaxrate;
-    data.taxburden= +data.taxburden;
-
-
-var xValue = country;
-var yValue = incometaxrate;
-var yValue2 = corptaxrate;
- var yValue3= taxburden;
-
-var trace1 = {
-  x: xValue,
-  y: yValue,
-  type: 'bar',
-  text: yValue.map(String),
-  textposition: 'auto',
-  hoverinfo: 'none',
-  opacity: 0.5,
-  marker: {
-    color: 'rgb(158,202,225)',
-    line: {
-      color: 'rgb(8,48,107)',
-      width: 1.5
-    }
-  }
-};
-
-var trace2 = {
-  x: xValue,
-  y: yValue2,
-  type: 'bar',
-  text: yValue2.map(String),
-  textposition: 'auto',
-  hoverinfo: 'none',
-  marker: {
-    color: 'rgba(58,200,225,.5)',
-    line: {
-      color: 'rgb(8,48,107)',
-      width: 1.5
-    }
-  }
-};
-
-var trace3 = {
-    x: xValue,
-    y: yValue3,
-    type: 'bar',
-    text: yValue3.map(String),
-    textposition: 'auto',
-    hoverinfo: 'none',
-    marker: {
+    var trace1 = {
+      x: country,
+      y: incometaxrate,
+      type: 'bar',
+      text: country.map(String),
+      textposition: 'auto',
+      hoverinfo: 'none',
+      opacity: 0.5,
+      marker: {
+        color: 'rgb(158,202,225)',
+        line: {
+          color: 'rgb(8,48,107)',
+          width: 1.5
+        }
+      }
+    };
+    
+    var trace2 = {
+      x: country,
+      y: corptaxrate,
+      type: 'bar',
+      text: country.map(String),
+      textposition: 'auto',
+      hoverinfo: 'none',
+      marker: {
         color: 'rgba(58,200,225,.5)',
         line: {
-        color: 'rgb(8,48,107)',
-        width: 1.5
+          color: 'rgb(8,48,107)',
+          width: 1.5
+        }
       }
-    }
-};
-var data = [trace1,trace2,trace3];
-
-var layout = {
-  title: 'Tax Burden vs Tax Rates in Each Country'
-};
-
-Plotly.newPlot('bar-plot', data, layout);
+    };
+    
+    var trace3 = {
+        x: country,
+        y: taxburden,
+        type: 'bar',
+        text: country.map(String),
+        textposition: 'auto',
+        hoverinfo: 'none',
+        marker: {
+            color: 'rgba(58,200,225,.5)',
+            line: {
+            color: 'rgb(8,48,107)',
+            width: 1.5
+          }
+        }
+    };
+    var data = [trace1,trace2,trace3];
+    
+    var layout = {
+      title: 'Tax Burden vs Tax Rates in Each Country'
+    };
+    
+    Plotly.newPlot('bar-plot', data, layout);
