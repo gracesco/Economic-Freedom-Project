742c4262a01ea90345ccb3ce3dfdb51cfab399ce
Grace Huefner <gscott156@gmail.com>
2020-07-14 15:44:56 -0500

    adding updated code for grace

--- assets/js/GDP.js
+++ assets/js/GDP.js
@@ -1,40 +1,41 @@
-// Create chart by defining margins and
-var svg = d3.select("#unemployment"),
+// Create chart by defining margins for master select #unemployment 
+var svg = d3.select("#unemployment"),    
     margin = {top: 20, right: 20, bottom: 30, left: 40},
     width = +svg.attr("width") - margin.left - margin.right,
     height = +svg.attr("height") - margin.top - margin.bottom,
     g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 // Create a Bandscale (continuous numeric scale for bandwidth) for the y-variable
-var y = d3.scaleBand()
-    .rangeRound([0, height])
+var y = d3.scaleBand()			
+    .rangeRound([0, height])	
     .paddingInner(0.05)
     .align(0.1);
 
 //Create a linear scale for the x-variable
-var x = d3.scaleLinear()
-    .rangeRound([0, width]);
+var x = d3.scaleLinear()		
+    .rangeRound([0, width]);	
 
 // Create an ordinal scale (layered scale) using specific colors to determine distribution of data
 var z = d3.scaleOrdinal()
-    .range(["#00bfff", "#8000ff", "#ff0080"]);
+    .range(["#fafa6e", "#2a4858", "#3fb78d", "#008d8c", "#255566"]);
 
-// Read in CSV and define what the column sizes will be from the data
-d3.csv("../Outputs/GDPStatsbyRegion.csv", function(d, i, columns) {
+
+// Read in CSV and define what the column sizes will be from the data   
+d3.csv("./Outputs/UnemploymentGDP.csv", function(d, i, columns) {
   for (i = 2, t = 0; i < columns.length; ++i) t += d[columns[i]] = +d[columns[i]];
   d.total = t;
   return d;
 }, function(error, data) {
   if (error) throw error;
-  // console.log(data)
+  console.log(data)
+  
+  // List the subgroups of the data 
+  var keys = data.columns.slice(2);
 
-  // List the subgroups of the data (headers)
-  var keys = data.columns.slice(1);
-
-
+// Sort data and map to the graph 
   data.sort(function(a, b) { return b.total - a.total; });
-  y.domain(data.map(function(d) { return d.Region; }));
-  x.domain([0, d3.max(data, function(d) { return d.total; })]).nice();
+  y.domain(data.map(function(d) { return d.Region; }));					
+  x.domain([0, d3.max(data, function(d) { return d.total; })]).nice();	
   z.domain(keys);
 
   g.append("g")
@@ -45,29 +46,39 @@ d3.csv("../Outputs/GDPStatsbyRegion.csv", function(d, i, columns) {
     .selectAll("rect")
     .data(function(d) { return d; })
     .enter().append("rect")
-      .attr("y", function(d) { return y(d.data.Region); })
-      .attr("x", function(d) { return x(d[0]); })
-      .attr("width", function(d) { return x(d[1]) - x(d[0]); })
-      .attr("height", y.bandwidth());
+      .attr("y", function(d) { return y(d.data.Region); })	    
+      .attr("x", function(d) { return x(d[0]); })			    
+      .attr("width", function(d) { return x(d[1]) - x(d[0]); })	
+      .attr("height", y.bandwidth())
+    .on("mouseover", function() { tooltip.style("display", null); })
+    .on("mouseout", function() { tooltip.style("display", "none"); })
+    .on("mousemove", function(d) {
+      console.log(d);
+      var xPosition = d3.mouse(this)[0] - 5;
+      var yPosition = d3.mouse(this)[1] - 5;
+      tooltip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
+      tooltip.select("text").text(d[1]-d[0]);
+    });						    	
 
   g.append("g")
       .attr("class", "axis")
-      .attr("transform", "translate(0,0)")
-      .call(d3.axisLeft(y));
+      .attr("transform", "translate(0,0)") 						
+      .call(d3.axisLeft(y));									
 
   g.append("g")
       .attr("class", "axis")
-	  .attr("transform", "translate(0,"+height+")")
-      .call(d3.axisBottom(x).ticks(null, "s"))
+	  .attr("transform", "translate(0,"+height+")")				
+      .call(d3.axisBottom(x).ticks(null, "s"))					
     .append("text")
-      .attr("y", 2)
-      .attr("x", x(x.ticks().pop()) + 0.5)
-      .attr("dy", "0.32em")
+      .attr("y", 2)												
+      .attr("x", x(x.ticks().pop()) + 0.5) 						
+      .attr("dy", "0.32em")										
       .attr("fill", "#000")
       .attr("font-weight", "bold")
       .attr("text-anchor", "start")
-	  .attr("transform", "translate("+ (-width) +",-10)");
+	  .attr("transform", "translate("+ (-width) +",-10)");   	
 
+// Add a legend     
   var legend = g.append("g")
       .attr("font-family", "sans-serif")
       .attr("font-size", 10)
@@ -75,7 +86,7 @@ d3.csv("../Outputs/GDPStatsbyRegion.csv", function(d, i, columns) {
     .selectAll("g")
     .data(keys.slice().reverse())
     .enter().append("g")
-	  .attr("transform", function(d, i) { return "translate(-50," + (300 + i * 20) + ")"; });
+	  .attr("transform", function(d, i) { return "translate(-50," + (500 + i * 20) + ")"; });
 
   legend.append("rect")
       .attr("x", width - 19)
@@ -89,3 +100,25 @@ d3.csv("../Outputs/GDPStatsbyRegion.csv", function(d, i, columns) {
       .attr("dy", "0.32em")
       .text(function(d) { return d; });
 });
+
+//add tooltip 
+var tooltip = svg.append("g")
+    .attr("class", "tooltip")
+    .style("display", "none");
+      
+  tooltip.append("rect")
+    .attr("width", 60)
+    .attr("height", 20)
+    .attr("fill", "white")
+    .style("opacity", 0.5);
+
+  tooltip.append("text")
+    .attr("x", 30)
+    .attr("dy", "1.2em")
+    .style("text-anchor", "middle")
+    .attr("font-size", "12px")
+    .attr("font-weight", "bold");
+
+
+
+
